7e37393e46ae9116e1feedc7b82cbb7d663b34e5
John Cintineo (Personal) <jlc248@gmail.com>
2026-06-29 13:29:46 -0500

    small updates

--- torcnn/recalibration.py
+++ torcnn/recalibration.py
@@ -9,14 +9,14 @@ import os
 import joblib
 
 # Load data 
-eval_data = '/work2/jcintineo/torcnn/tests/2011-24/test01/eval2018/eval_data.csv'
+eval_data = '/work2/jcintineo/torcnn/tests/2011-19/test07/eval2018/eval_data.csv'
 df = pd.read_csv(eval_data)
 
 y_true = np.array(df.tornado)
 y_prob = np.array(df.prob)
 
-isotonic = False
-spline = True
+isotonic = True 
+spline = False
 # If both are False, 
 # then Platt scaling is used
 
@@ -71,12 +71,15 @@ print(f"Brier Score Before: {brier_before:.5f}")
 print(f"Brier Score After:  {brier_after:.5f}")
 
 # Find the new Optimal Threshold for CSI
+precision_orig, recall_orig, thresholds_orig = precision_recall_curve(y_true, y_prob)
 precision, recall, thresholds = precision_recall_curve(y_true, y_prob_calibrated)
 # Assuming CSI formula: CSI = 1 / (1/prec + 1/rec - 1)
+csi_orig = 1 / (1/precision_orig + 1/recall_orig - 1 + 1e-10)
+ix_orig = np.argmax(csi_orig)
 csi = 1 / (1/precision + 1/recall - 1 + 1e-10)
 ix = np.argmax(csi)
-
-print(f"ALL New Max CSI: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}")
+print(f"ALL Max CSI before: {csi_orig[ix_orig]:.4f} at Threshold: {thresholds_orig[ix_orig]:.4f}")
+print(f"ALL Max CSI after: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}")
 
 # For notor vs pretor only
 df2 = df[(df.pretorMinutes > 0) | (df.tornado == 0)]
@@ -86,10 +89,15 @@ if isotonic:
     y_prob_calibrated = calibrator.transform(y_prob)
 else:
     y_prob_calibrated = calibrator.predict_proba(y_prob.reshape(-1, 1))[:, 1]
+precision_orig, recall_orig, thresholds_orig = precision_recall_curve(y_true, y_prob)
+csi_orig = 1 / (1/precision_orig + 1/recall_orig - 1 + 1e-10)
+ix_orig = np.argmax(csi_orig)
 precision, recall, thresholds = precision_recall_curve(y_true, y_prob_calibrated)
 csi = 1 / (1/precision + 1/recall - 1 + 1e-10)
 ix = np.argmax(csi)
-print(f"Pretor vs. nontor New Max CSI: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}\n")
+print(f"Pretor vs. nontor Max CSI before: {csi_orig[ix_orig]:.4f} at Threshold: {thresholds_orig[ix_orig]:.4f}")
+print(f"Pretor vs. nontor Max CSI after: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}\n")
+
 
 # For notor vs 1-10 min pretor only
 df2 = df[((df.pretorMinutes > 0) & (df.pretorMinutes <= 10)) | (df.tornado == 0)]
@@ -99,10 +107,15 @@ if isotonic:
     y_prob_calibrated = calibrator.transform(y_prob)
 else:
     y_prob_calibrated = calibrator.predict_proba(y_prob.reshape(-1, 1))[:, 1]
+precision_orig, recall_orig, thresholds_orig = precision_recall_curve(y_true, y_prob)
+csi_orig = 1 / (1/precision_orig + 1/recall_orig - 1 + 1e-10)
+ix_orig = np.argmax(csi_orig)
 precision, recall, thresholds = precision_recall_curve(y_true, y_prob_calibrated)
 csi = 1 / (1/precision + 1/recall - 1 + 1e-10)
 ix = np.argmax(csi)
-print(f"Pretor (1-10 min) vs. nontor New Max CSI: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}\n")
+print(f"Pretor (1-10 min) vs. nontor Max CSI before: {csi_orig[ix_orig]:.4f} at Threshold: {thresholds_orig[ix_orig]:.4f}")
+print(f"Pretor (1-10 min) vs. nontor Max CSI after: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}\n")
+
 
 # For notor vs 11-20 min pretor only
 df2 = df[((df.pretorMinutes > 10) & (df.pretorMinutes <= 20)) | (df.tornado == 0)]
@@ -112,10 +125,15 @@ if isotonic:
     y_prob_calibrated = calibrator.transform(y_prob)
 else:
     y_prob_calibrated = calibrator.predict_proba(y_prob.reshape(-1, 1))[:, 1]
+precision_orig, recall_orig, thresholds_orig = precision_recall_curve(y_true, y_prob)
+csi_orig = 1 / (1/precision_orig + 1/recall_orig - 1 + 1e-10)
+ix_orig = np.argmax(csi_orig)
 precision, recall, thresholds = precision_recall_curve(y_true, y_prob_calibrated)
 csi = 1 / (1/precision + 1/recall - 1 + 1e-10)
 ix = np.argmax(csi)
-print(f"Pretor (11-20 min) vs. nontor New Max CSI: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}\n")
+print(f"Pretor (11-20 min) vs. nontor Max CSI before: {csi_orig[ix_orig]:.4f} at Threshold: {thresholds_orig[ix_orig]:.4f}")
+print(f"Pretor (11-20 min) vs. nontor Max CSI after: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}\n")
+
 
 # For notor vs 21-30 min pretor only
 df2 = df[((df.pretorMinutes > 20) & (df.pretorMinutes <= 30)) | (df.tornado == 0)]
@@ -125,7 +143,12 @@ if isotonic:
     y_prob_calibrated = calibrator.transform(y_prob)
 else:
     y_prob_calibrated = calibrator.predict_proba(y_prob.reshape(-1, 1))[:, 1]
+precision_orig, recall_orig, thresholds_orig = precision_recall_curve(y_true, y_prob)
+csi_orig = 1 / (1/precision_orig + 1/recall_orig - 1 + 1e-10)
+ix_orig = np.argmax(csi_orig)
 precision, recall, thresholds = precision_recall_curve(y_true, y_prob_calibrated)
 csi = 1 / (1/precision + 1/recall - 1 + 1e-10)
 ix = np.argmax(csi)
-print(f"Pretor (21-30 min) vs. nontor New Max CSI: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}")
+print(f"Pretor (21-30 min) vs. nontor Max CSI before: {csi_orig[ix_orig]:.4f} at Threshold: {thresholds_orig[ix_orig]:.4f}")
+print(f"Pretor (21-30 min) vs. nontor Max CSI after: {csi[ix]:.4f} at Threshold: {thresholds[ix]:.4f}\n")
+
